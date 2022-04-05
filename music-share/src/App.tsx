4de3662b5e0f3102a8dcffc483163742b1d1d991
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-04-05 21:53:03 +0700

    Task#6: Done builing layout music sharing app

--- music-share/src/App.tsx
+++ music-share/src/App.tsx
@@ -1,23 +1,61 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, useMediaQuery, Hidden } from "@material-ui/core";
 
 import Header from "./components/Header";
-import SongPlay from "./components/Song/SongPlay";
+import SongPlayer from "./components/Song/SongPlayer";
 import AddSong from "./components/Song/AddSong";
 import SongList from "./components/Song/SongList";
 
 function App() {
+  const getterThanSm = useMediaQuery((theme: any) =>
+    theme.breakpoints.up("sm")
+  );
+
+  const getterThanMd = useMediaQuery((theme: any) =>
+    theme.breakpoints.up("md")
+  );
+
   return (
     <>
-      <Header />
+      <Hidden only="xs">
+        <Header />
+      </Hidden>
+      {/* {getterThanSm && <Header />} */}
       <Grid container spacing={3}>
-        <Grid item xs={12} md={7}>
+        <Grid
+          item
+          xs={12}
+          md={7}
+          style={{
+            padding: getterThanSm ? 80 : 10,
+          }}
+        >
           <AddSong />
           <SongList />
         </Grid>
 
-        <Grid item xs={12} md={5}>
-          <SongPlay />
+        <Grid
+          item
+          xs={12}
+          md={5}
+          style={
+            getterThanMd
+              ? {
+                  position: "fixed",
+                  width: "100%",
+                  right: "0",
+                  top: "70",
+                  paddingTop: 80,
+                }
+              : {
+                  position: "fixed",
+                  left: 0,
+                  bottom: 0,
+                  width: "100%",
+                }
+          }
+        >
+          <SongPlayer />
         </Grid>
       </Grid>
     </>
--- music-share/src/components/Song/SongList.tsx
+++ music-share/src/components/Song/SongList.tsx
@@ -1,7 +1,100 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Typography,
+  IconButton,
+  makeStyles,
+} from "@material-ui/core";
+
+import { SongType } from "../../type";
+import { PlayArrow, Save } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: theme.spacing(3),
+  },
+  songInfoContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  songInfo: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  songThumbnail: {
+    objectFit: "cover",
+    width: 140,
+    height: 140,
+  },
+}));
 
 const SongList = () => {
-  return <div>SongList</div>;
+  let loading = false;
+
+  const song = {
+    title: "Love",
+    artist: "Moon",
+    thumbnail:
+      "https://bestlifeonline.com/wp-content/uploads/sites/3/2019/03/Earbuds-against-heart-background.jpg",
+  };
+
+  if (loading)
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  return (
+    <div>
+      {Array.from({ length: 10 }, () => song).map((song, index) => (
+        <Song key={index} song={song} />
+      ))}
+    </div>
+  );
+};
+
+const Song = ({ song }: any) => {
+  const classes = useStyles();
+  const { title, thumbnail, artist } = song;
+
+  return (
+    <Card className={classes.container}>
+      <div className={classes.songInfoContainer}>
+        <CardMedia image={thumbnail} className={classes.songThumbnail} />
+        <div className={classes.songInfo}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body1" component="p" color="textSecondary">
+              {artist}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton size="small" color="primary">
+              <PlayArrow />
+            </IconButton>
+
+            <IconButton size="small" color="secondary">
+              <Save color="secondary" />
+            </IconButton>
+          </CardActions>
+        </div>
+      </div>
+    </Card>
+  );
 };
 
 export default SongList;
