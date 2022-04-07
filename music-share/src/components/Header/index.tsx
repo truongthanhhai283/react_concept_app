b58a8cba005e22ec7fb5e3f8663b33522354d5dd
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-04-07 18:27:47 +0700

    Task#11: Update time

--- music-share/src/components/Header/index.tsx
+++ music-share/src/components/Header/index.tsx
@@ -1,22 +1,53 @@
-import React from "react";
-import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import {
+  AppBar,
+  makeStyles,
+  Toolbar,
+  Typography,
+  Tooltip,
+  Box,
+  IconButton,
+  Avatar,
+} from "@material-ui/core";
 import { HeadsetTwoTone } from "@material-ui/icons";
+
+import { useDate } from "../../getDay";
 
 const useStyles = makeStyles((theme) => ({
   title: {
     marginLeft: theme.spacing(2), // ~ '8px'
   },
+  left: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
 }));
 
 const Header = () => {
+  const { date, time, wish } = useDate();
+
   const classes = useStyles();
   return (
     <AppBar color="secondary" position="fixed">
-      <Toolbar>
+      <Toolbar className={classes.toolbar}>
         <HeadsetTwoTone />
-        <Typography className={classes.title} variant="h6" component="h1">
-          Apollo music share
-        </Typography>
+        <div className={classes.left}>
+          <Typography className={classes.title} variant="h6" component="h1">
+            {wish}
+          </Typography>
+          <span>
+            {date}, {time}
+          </span>
+        </div>
+
+        <Box sx={{ flexGrow: 0 }}>
+          <p>Trương Thanh Hải</p>
+        </Box>
       </Toolbar>
     </AppBar>
   );
